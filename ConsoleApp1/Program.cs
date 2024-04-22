808eb4581e45eeb709e41180f4ad2a13e8b22819
Илья <@DESKTOP-1OGUEKL>
2024-04-22 18:27:37 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using static System.Console;
+
+namespace ConsoleApp1
+{
+    /// Первая практика
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Первое задание
+            WriteLine("Hello World");
+            //Второе задание
+            Write("Hello ");
+            Write("World");
+            Write("!!!");
+            ReadLine();//В отличии от ReadKey Позволяет закрыть консоль только при нажатии Enter
+        }      
+    }
+}
